6affdefecacfa00084369b88fbf5da5250c1dc79
JeremiasMora <jeremias.morac@gmail.com>
2021-07-17 20:17:32 -0400

    test serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+//use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,9 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /*
         if(env('APP_ENV') !== 'local')
         {
             $url->forceSchema('https');
         }
+        */
     }
 }
