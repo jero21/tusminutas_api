2e9a37077b40edcc5fadf92857fe503f6b786637
JeremiasMora <jeremias.morac@gmail.com>
2021-07-17 21:02:34 -0400

    test appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!App::environment([
-            'local',
-            'testing',
-        ])) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
