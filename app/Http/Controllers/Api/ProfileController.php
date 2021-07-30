25e8d3a875b9a6066a268ff04c9c1aabff528a89
JeremiasMora <jeremias.morac@gmail.com>
2021-07-29 23:39:41 -0400

    crear actualizar perfil

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Profile;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
 use App\Models\LenguageLevel;
 use App\Models\OtherStudies;
-use Illuminate\Http\Request;
-use App\Models\Lenguage;
-use App\Models\TypeRrss;
+use App\Models\Profile;
 use App\Models\Rrss;
 
 class ProfileController extends Controller
@@ -24,14 +23,81 @@ class ProfileController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created and uptate resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+      $user_auth = JWTAuth::parseToken()->authenticate();
+        
+      DB::transaction(function() use ($request, $user_auth) {
+
+        // Profile
+        if ($request->id_profile) {
+          $profile = Profile::find($request->id_profile);
+        } else {
+          $profile = new Profile();
+        }
+        $profile->profesion       = $request->profesion;
+        $profile->presentacion    = $request->presentacion;
+        $profile->ocupacion       = $request->ocupacion;
+        $profile->especializacion = $request->especializacion;
+        $profile->witio_web       = $request->sitio_web;
+        $profile->telefono        = $request->telefono;
+        $profile->save();
+
+        // User
+        $user = User::find($user_auth->id);
+        $user->nombre = $request->nombre;
+        $user->email = $request->email;
+        if ($profile->id) {
+          $user->id_profile = $profile->id;
+        }
+        $user->save();
+
+        // LenguageLevel
+        $lenguages = $request->lenguages_level;
+        foreach ($lenguages as $key => $lenguage) {
+          $leguage_level = LenguageLevel::find($lenguage->id)
+          if (isset($leguage_level)) {
+            $leguage_level = new LenguageLevel();
+          }
+          $leguage_level->nivel_manejo  = $request->nivel_manejo;
+          $leguage_level->id_lenguage   = $request->id_lenguage;
+          $leguage_level->id_profile    = $profile->id;
+          $leguage_level->save();
+        }
+
+        // RRSS
+        $redes_sociales = $request->redes_sociales;
+        foreach ($redes_sociales as $key => $red_social) {
+          $rrss = Rrss::find($red_social->id)
+          if (isset($rrss)) {
+            $rrss = new Rrss();
+          }
+          $rrss->url          = $red_social->url;
+          $rrss->id_type_rrss = $red_social->id_type_rrss;
+          $rrss->id_profile   = $profile->id;
+          $rrss->save();
+        }
+
+        // OTHER STUDIES
+        $other_studies = $request->other_studies;
+        foreach ($other_studies as $key => $studie) {
+          $other_studie = OtherStudies::find($studie->id)
+          if (isset($other_studie)) {
+            $other_studie = new OtherStudies();
+          }
+          $other_studie->nombre  = $studie->nombre;
+          $other_studie->lugar   = $studie->lugar;
+          $other_studie->id_profile      = $profile->id;
+          $other_studie->save();
+        }
+
+      });
+      return \Response::json(['create' => true], 200);
     }
 
     /**
@@ -72,13 +138,5 @@ class ProfileController extends Controller
     public function destroy($id)
     {
         //
-    }
-
-    public function typeRrss () {
-        return TypeRrss::all();
-    }
-
-    public function lenguage () {
-        return Lenguage::all();
     }
 }
